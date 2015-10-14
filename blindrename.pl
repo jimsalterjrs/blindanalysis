fd9515ebae4b6873cc39b1b94d55c6a2eba0a98a
Jim Salter <github@jrs-s.net>
2015-10-14 12:44:42 -0400

    looks pretty good from here.

--- blindrename.pl
+++ blindrename.pl
@@ -7,7 +7,7 @@
 
 my $folder = $ARGV[0];
 
-if ($folder == '') {
+if ($folder eq '') {
 	print "You must supply a folder name.  I will randomly rename all files in that foldername, and create a key file in .CSV format in that folder.\n";
 	exit 1;
 }
@@ -22,10 +22,32 @@ if (-e "$folder/keyfile.csv") {
 	exit 3;
 }
 
-open FH, "> $folder/keyfile.csv";
+open FH, "> $folder/keyfile.csv" or die "could not open $folder/keyfile.csv for writing!\n";
+
+my @chars=("A"..."Z","a"..."z","0"..."9");
 
 foreach my $file (<$folder/*>) {
-	print $file;
+	chomp $file;
+	my ($oldname) = $file =~ /^$folder\/(.*)$/;
+	# don't mess with keyfile.csv itself
+	if ($oldname ne 'keyfile.csv') {
+		# separate extension from basename. be sure you handle bare basenames (no extension) properly if necessary
+		my ($ext) = $oldname =~ /^.*(\..*)$/;
+		my ($base) = $oldname =~ /^(.*)($ext)?$/;
+
+		# generate a random new basename - 5 alphanumerics. ensure there are no collisions.
+		my $cloaked = $folder;
+		while (-e $cloaked) {
+			$cloaked = '';
+			for (1..5) { $cloaked .= $chars[int rand scalar @chars]; }
+		}
+
+		my $cloakedname = "$cloaked$ext";
+
+		# first log rename operation, THEN perform it
+		print FH "\"$oldname\",\"$cloakedname\"\n";
+		rename "$folder/$oldname","$folder/$cloakedname" or die "Could not rename $folder/$oldname to $folder/$cloakedname - you may have a partial operation, check $folder/keyfile.csv to see details!\n";
+	}
 }
 
 close FH;
